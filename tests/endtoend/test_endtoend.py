f3c28ab86994d9ad2468a37352fc0023ba9afd15
mroy37 <madhu.roy@aexp.com>
2021-02-16 17:03:34 +0000

    Addressed review comments

--- tests/endtoend/test_endtoend.py
+++ tests/endtoend/test_endtoend.py
@@ -59,6 +59,10 @@ def test_task_journey(driver, test_app):
     driver.get('http://127.0.0.1:5000/')
     assert driver.title == 'To-Do App'
 
+    todo_name = driver.find_element_by_id("todoname").text
+    todo_desc = driver.find_element_by_id("tododesc").text
+    todo_status = driver.find_element_by_id("todostatus").text
+
     # Create new item
     els = driver.find_elements_by_tag_name("td")
     driver.find_element_by_id("name_input").send_keys("Watch movie")
@@ -66,9 +70,9 @@ def test_task_journey(driver, test_app):
     driver.find_element_by_id("add-item").click()
     driver.implicitly_wait(2)
     els = driver.find_elements_by_tag_name("td")
-    assert driver.find_element_by_id("todoname").text == "Watch movie"
-    assert driver.find_element_by_id("tododesc").text == "Movie on TV"
-    assert driver.find_element_by_id("todostatus").text == "To Do"
+    assert todo_name == "Watch movie"
+    assert todo_desc == "Movie on TV"
+    assert todo_status == "To Do"
 
     #Start item
     driver.find_element_by_id("start-btn").click()
@@ -87,9 +91,9 @@ def test_task_journey(driver, test_app):
     #Undo item
     driver.find_element_by_id("undo-btn").click()
     driver.implicitly_wait(2)
-    assert driver.find_element_by_id("todoname").text == "Watch movie"
-    assert driver.find_element_by_id("tododesc").text == "Movie on TV"
-    assert driver.find_element_by_id("todostatus").text == "To Do"
+    assert todo_name == "Watch movie"
+    assert todo_desc == "Movie on TV"
+    assert todo_status == "To Do"
 
 
 def create_trello_board(name):
